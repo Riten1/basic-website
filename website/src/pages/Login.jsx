2f5c5ae210c51f6c36c247b007a787d6868796f1
Riten1 <122423532+Riten1@users.noreply.github.com>
2023-11-10 08:23:40 +0545

    'AddedGlobalVariable'

--- website/src/pages/Login.jsx
+++ website/src/pages/Login.jsx
@@ -1,15 +1,23 @@
 import React, { useState } from 'react'
+import useUser from '../context/UserContext';
+
 
 const Login = () => {
   const [userName, setUsername] = useState('');
   const [password, setPassword] = useState('')
+
+  const {setUser} = useUser()
+
+   function onSubmit(){
+      setUser({userName, password})
+  }
 
   return (
     <div>
       <h1>Login</h1>
       <input type='text' placeholder='Username' value={userName} onChange={(e) => {setUsername(e.target.value)}}></input>
       <input type='text' placeholder='Password' value={password} onChange={(e) => {setPassword(e.target.value)}}></input>
-      <button></button>
+      <button onClick={onSubmit}>Submit</button>
 
     </div>
   )
